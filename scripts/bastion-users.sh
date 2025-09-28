0e5b7c4a811ec456a33208aa78288e62a853fb71
joek <sleepyjoeyu@gmai.com>
2025-09-29 01:49:47 +0800

    update scripts/bastion-users.sh

--- scripts/bastion-users.sh
+++ scripts/bastion-users.sh
@@ -5,6 +5,6 @@ for i in `seq 1 3`;do
   echo redhat | passwd user$i --stdin
 done
 
-echo 'PS1="\[\e[1;31;47m\][\u\W]\$ \[\e[0m\]"' >> /home/user$i/.bashrc
-echo 'PS1="\[\e[1;30;47m\][\u\W]\$ \[\e[0m\]"' >> /home/user$i/.bashrc
-echo 'PS1="\[\e[1;39;45m\][\u\W]\$ \[\e[0m\]"' >> /home/user$i/.bashrc
+echo 'PS1="\[\e[1;31;47m\][\u\W]\$ \[\e[0m\]"' >> /home/user1/.bashrc
+echo 'PS1="\[\e[1;30;47m\][\u\W]\$ \[\e[0m\]"' >> /home/user2/.bashrc
+echo 'PS1="\[\e[1;39;45m\][\u\W]\$ \[\e[0m\]"' >> /home/user3/.bashrc
